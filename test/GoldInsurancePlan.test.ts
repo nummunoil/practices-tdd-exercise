7bd512ee79482b1f8c36c6c12030e5eb10d63199
Oil <sudarat@1moby.com>
2023-12-27 11:43:32 +0700

    step 10

--- test/GoldInsurancePlan.test.ts
+++ test/GoldInsurancePlan.test.ts
@@ -13,4 +13,16 @@ describe("Gold and fund is 500K", () => {
     numOfYears = 2;
     expect(insurancePlan.calculateMoneyBack(numOfYears)).toEqual(3000);
   });
+
+  test.each([
+    { numOfYears: 0, amount: 0 },
+    { numOfYears: 1, amount: 0 },
+    { numOfYears: 2, amount: 3000 },
+    { numOfYears: 3, amount: 500000 },
+  ])(
+    "num of years is $numOfYears should return $amount",
+    ({ numOfYears, amount }) => {
+      expect(insurancePlan.calculateMoneyBack(numOfYears)).toEqual(amount);
+    }
+  );
 });
